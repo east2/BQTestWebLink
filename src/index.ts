21b561b9fbbed9ce003f50d53ca865564ed43866
Edward Fisher <e.fisher@computer.org>
2023-10-27 16:10:27 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,18 @@
+import { selectInputElement } from '@finsweet/ts-utils';
+
 import { greetUser } from '$utils/greet';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
-  const name = 'John Doe';
-  greetUser(name);
+  const defaultId = document.getElementById('default-id');
+  if (defaultId) {
+    defaultId.onclick = function () {
+      const fn = document.getElementById('fieldName');
+      if (fn) {
+        (<HTMLInputElement>fn).value = 'This is on my machine.';
+      }
+    };
+  }
 });
+
+// <script>document.getElementById('default-id').onclick = function() { document.getElementById('fieldName').value = 'clicked'; }</script>
