dceca64559bd5e5966fbe9bb1e7c4cb1f66058ae
Edward Fisher <e.fisher@computer.org>
2023-11-03 10:33:43 -0400

    fixes bug in value

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,26 @@
-"use strict";(()=>{window.Webflow||(window.Webflow=[]);window.Webflow.push(()=>{let e=document.getElementById("default-id");e&&(e.onclick=function(){let t=document.getElementById("field-name-notsure");if(t){t.value=user.getUsername();try{throw alert("in try."),new Error("too dumb")}catch(o){alert(o.stack)}}})});})();
+"use strict";
+(() => {
+  // bin/live-reload.js
+  new EventSource(`${"http://localhost:3000"}/esbuild`).addEventListener("change", () => location.reload());
+
+  // src/index.ts
+  window.Webflow ||= [];
+  window.Webflow.push(() => {
+    const defaultId = document.getElementById("default-id");
+    if (defaultId) {
+      defaultId.onclick = function() {
+        const fn = document.getElementById("field-name-notsure");
+        if (fn) {
+          fn.value = "a new test";
+          try {
+            alert("in try.");
+            throw new Error("too dumb");
+          } catch (error) {
+            alert(error.stack);
+          }
+        }
+      };
+    }
+  });
+})();
+//# sourceMappingURL=index.js.map
