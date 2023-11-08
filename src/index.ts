2e26bfc7756287252a74f94d3a8f86a7232a3b53
Edward Fisher <e.fisher@computer.org>
2023-11-08 09:56:22 -0500

    remove commented out code

--- src/index.ts
+++ src/index.ts
@@ -29,35 +29,6 @@ const init = () => {
 
         const test = await response.json();
         cookies().set("accessToken", test);
-
-        // fetch('https://yfjqz30vcg.execute-api.us-east-2.amazonaws.com/default/bq_login', {
-        //   mode: 'no-cors',
-        //   method: 'POST',
-        //   body: JSON.stringify({
-        //     'user_name': 'e.fisher@computer.org',
-        //     'password': 'pz8882Visa!',
-        //   }),
-        //   headers: {
-        //     'Content-type': 'application/json',
-        //     'Access-Control-Allow-Origin': '*',
-        //   },
-        // })
-        //   .then((response) => response.json())
-        //   .then((responseJson) => { alert(responseJson); });
-        // .then((response) => {
-        //   if (!response.ok) {
-        //     throw new Error(`HTTP error! Status: ${response.status}`);
-        //   } else {
-        //     alert(response.statusText);
-        //   }
-        //   return response;
-        // })
-        // .then((data) => {
-        //   alert('Response data: ' + data);
-        // })
-        // .catch((error) => {
-        //   alert('Fetch error: ' + error);
-        // });
       }
     };
   }
@@ -65,7 +36,3 @@ const init = () => {
 
 window.Webflow ||= [];
 window.Webflow.push(init);
-
-// init();
-
-// <script>document.getElementById('default-id').onclick = function() { document.getElementById('fieldName').value = 'clicked'; }</script>
